f64e7effb04f38093e43c0c93ed0d029ab608608
Erin Parker <erinchanparker@gmail.com>
2013-12-07 03:20:48 -0800

    removed cardmaker.rb and other images

--- cardmaker.rb
+++
@@ -1,9 +0,0 @@
-require "rmagick"
-
-image1 = Magick::Image.read("SaraBlakely.jpg").first
-image2 = Magick::Image.read("Supergirl.png").first
-image2 = image1.append(true)
-image2.write("final.png");
-image2.display
-
-
